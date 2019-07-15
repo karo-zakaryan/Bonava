59e9a2cbed3babef5aed1d5033c362be22d10632
Karo Zakaryan <karo@devduo.com>
2019-07-15 15:37:35 +0400

    Disable devTool

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,7 @@
 import thunk from "redux-thunk";
 import reducers from "./reducers/rootReducer/rootReducer";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
-const composeEnhancers =
-  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
-
-const devTools =
-    process.env.NODE_ENV === "production"
-        ? applyMiddleware(thunk)
-        : composeEnhancers(applyMiddleware(thunk));
-
-const store = createStore(reducers,devTools);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 export default store;
